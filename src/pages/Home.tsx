d47ae01b6fdcf3f78644e02fc04966c6de25aaaa
Anirudh Memani <nemanianirudh@gmail.com>
2024-02-22 05:45:40 +0530

    intro added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,22 @@
-const Home = () => {
+// @ts-expect-error "video does not have types"
+import Intro from "../assets/videos/xlabs_intro.mp4";
+// @ts-expect-error "image does not have types"
+import Thumbnail from "../assets/images/xlabs_thumbnail.png";
+
+const Home: React.FC = (): React.JSX.Element => {
   return (
-    <div className="flex h-dvh items-center justify-center bg-black text-white">
-      Home
-    </div>
+    <section className="min-h-dvh bg-black text-white">
+      <div className="w-full">
+        <video
+          src={Intro}
+          autoPlay={true}
+          loop={true}
+          muted
+          poster={Thumbnail}
+          className="w-full"
+        />
+      </div>
+    </section>
   );
 };
 
